915f8f7955a431ef59dfac1b1bf5fafe7c0fbdd8
Suomaa <a.antomonov@mail.ru>
2017-10-19 17:54:08 +0300

    + Added an About activity + Added an App Menu + Added a icons for a menu About item

--- app/src/main/java/fi/suomaafrontieroy/aflashlight/MainActivity.java
+++ app/src/main/java/fi/suomaafrontieroy/aflashlight/MainActivity.java
@@ -20,6 +20,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -76,6 +78,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState (Bundle savedInstanceState) {
         savedInstanceState.putBoolean("isLightOn", isLightOn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_about:
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     @Override
